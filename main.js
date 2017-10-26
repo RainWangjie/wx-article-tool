52f256ecea3aaa440a9135ffefaa93b9e3625f9e
Mr.Rain <1148609893@qq.com>
2017-10-26 21:54:45 +0800

    初步版本

--- main.js
+++ main.js
@@ -1,60 +1,173 @@
-const electron = require('electron')
-// Module to control application life.
-const app = electron.app
-// Module to create native browser window.
-const BrowserWindow = electron.BrowserWindow
+const $ = require('jquery');
+const http = require('http');
+const https = require('https');
 
-const path = require('path')
-const url = require('url')
+const fs = require('fs');
+const cheerio = require('cheerio');
 
-// Keep a global reference of the window object, if you don't, the window will
-// be closed automatically when the JavaScript object is garbage collected.
-let mainWindow
+const localPath = './images/';
+const OSS = require('ali-oss').Wrapper;
 
-function createWindow () {
-  // Create the browser window.
-  mainWindow = new BrowserWindow({width: 800, height: 600})
+//oss 配置
+const ossConfig = require('./oss-config.json');
 
-  // and load the index.html of the app.
-  mainWindow.loadURL(url.format({
-    pathname: path.join(__dirname, 'index.html'),
-    protocol: 'file:',
-    slashes: true
-  }))
+let client = new OSS(ossConfig);
+// 存储文章标题
+let articleTitle = '';
 
-  // Open the DevTools.
-  // mainWindow.webContents.openDevTools()
+// 微信文章容器
+const $wx = $('.rich_media_area_primary');
 
-  // Emitted when the window is closed.
-  mainWindow.on('closed', function () {
-    // Dereference the window object, usually you would store windows
-    // in an array if your app supports multi windows, this is the time
-    // when you should delete the corresponding element.
-    mainWindow = null
-  })
+// 打印窗
+const $console = $('#console-wrapper ul');
+
+// 辅助函数
+let util = {
+    console(text) {
+        $console.append(`<li>${text}</li>`)
+    },
+    selectDownloadType(src) {
+        return src.substr(0, 5) === 'https' ? https : http
+    }
+};
+
+// 打开微信文章
+$('.btn-open').on('click', () => {
+    let url = $('[name="article-url" ]').val();
+    if (!url) {
+        alert('输入url');
+        return;
+    }
+
+    util.console('开始获取文章');
+    getArticle(url).then((html) => {
+        //采用cheerio模块解析html
+        let $cheerio = cheerio.load(html, {decodeEntities: false});
+        articleTitle = $cheerio('title').html();
+        $wx.html($cheerio('.rich_media_area_primary').html());
+        preLoadImage();
+    })
+});
+
+// 获取文章
+function getArticle(url) {
+    return new Promise(function (resolve, reject) {
+        util.selectDownloadType(url)
+            .get(url, function (res) {
+                let html = '';        //用来存储请求网页的整个html内容
+                res.setEncoding('UTF-8'); //防止中文乱码
+                //监听data事件，每次取一块数据
+                res.on('data', function (chunk) {
+                    html += chunk;
+                });
+                //监听end事件，如果整个网页内容的html都获取完毕，就执行回调函数
+                res.on('end', function () {
+                    util.console('获取文章成功');
+                    resolve(html);
+                });
+
+            })
+            .on('error', function (err) {
+                reject(err);
+                console.log(err);
+            });
+    });
+
 }
 
-// This method will be called when Electron has finished
-// initialization and is ready to create browser windows.
-// Some APIs can only be used after this event occurs.
-app.on('ready', createWindow)
+// 预加载微信图片
+function preLoadImage() {
+    $wx.find('img').each(function () {
+        $(this).attr('src', $(this).attr('data-src'))
+    })
+}
 
-// Quit when all windows are closed.
-app.on('window-all-closed', function () {
-  // On OS X it is common for applications and their menu bar
-  // to stay active until the user quits explicitly with Cmd + Q
-  if (process.platform !== 'darwin') {
-    app.quit()
-  }
-})
+// 替换图片
+$('.btn-capture-img').on('click', captureImg);
 
-app.on('activate', function () {
-  // On OS X it's common to re-create a window in the app when the
-  // dock icon is clicked and there are no other windows open.
-  if (mainWindow === null) {
-    createWindow()
-  }
-})
+function captureImg() {
+    let promiseList = [];
+    $wx.find('img').each(function (index, el) {
+        promiseList.push(captureImgItem(el, index))
+    });
+    Promise.all(promiseList).then(() => {
+        util.console('图片全部下载完成！！！');
+    });
+}
 
-// In this file you can include the rest of your app's specific main process
-// code. You can also put them in separate files and require them here.
+function captureImgItem(el, index) {
+    return downloadImg(el, index).then(updateImg, (index) => {
+        console.log(`图片${index}无效`);
+    }).then(changeImg).catch((err) => {
+        console.log(err)
+    });
+}
+
+// 下载图片
+function downloadImg(el, index) {
+    return new Promise(function (resolve, reject) {
+        let src = $(el).attr('data-src'),
+            name = `${new Date().getTime()}${Math.random().toString().substr(2, 6)}`,
+            type = $(el).attr('data-type'),
+            imgLocalPath = `${localPath}${name}.${type}`;
+
+        if (!src) {
+            reject(index);
+            return;
+        }
+
+        util.console(`图片${index}下载..`);
+        util.selectDownloadType(src).get(src, function (res) {
+            let file = fs.createWriteStream(imgLocalPath);
+            res.pipe(file);
+            res.on('end', function () {
+                util.console(`图片${index}下载完成`);
+                resolve({
+                    el,
+                    index,
+                    path: imgLocalPath,
+                    name
+                });
+            })
+        });
+    })
+}
+
+// 上传图片
+function updateImg(obj) {
+    console.log(obj.path);
+    util.console(`上传图片${obj.index}...`);
+    return client.put(`/article/${obj.name}`, obj.path).then(function (val) {
+        console.log(val.res.requestUrls[0]);
+        obj.cloudPath = val.res.requestUrls[0];
+        return obj;
+    });
+}
+
+//替换图片
+function changeImg(obj) {
+    util.console(`图片${obj.index}上传成功`);
+    $(obj.el).attr('src', obj.cloudPath).attr('data-src', '');
+}
+
+// 文章保存到本地
+$('.btn-save').on('click', saveFile);
+
+function saveFile() {
+    let tplHTML = $('#tpl').html();
+    // 标题
+    tplHTML = tplHTML.replace('{0}', articleTitle);
+
+    //内容
+    tplHTML = tplHTML.replace('{1}', $('#article-preview').html());
+
+    let file_name = `./html/${articleTitle}.html`;
+    fs.writeFile(file_name, tplHTML, 'utf8', function (err) {
+        if (err) {
+            alert(err);
+        }
+        util.console(`${articleTitle}文章保存成功！！！`);
+        alert(`${articleTitle}文章保存成功！！！`);
+    });
+}
+
